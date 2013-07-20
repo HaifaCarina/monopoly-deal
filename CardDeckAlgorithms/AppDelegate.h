73bce5ce838f73a6e4b5b1188bc3abb2e87263c0
Haifa Carina <haifacarina@00-88-65-42-d2-98.connectify>
2013-07-21 03:49:17 +0800

    Initial Commit

---
+++ CardDeckAlgorithms/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CardDeckAlgorithms
+//
+//  Created by Haifa Carina on 7/21/13.
+//  Copyright (c) 2013 Haifa Carina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CardDeckAlgorithms/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CardDeckAlgorithms
+//
+//  Created by Haifa Carina on 7/21/13.
+//  Copyright (c) 2013 Haifa Carina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
